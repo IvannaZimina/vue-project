d187a7f01653191268117f0f7da1e4bb31d46409
Zimina Ivanna <ivanna.zimina@plus4u.net>
2026-03-25 17:24:44 +0200

    Add documentation comments to main.js entry point

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
+// Import main stylesheet with Tailwind CSS directives
 import './assets/main.css'
+
+// Import Vue application factory function
 import { createApp } from 'vue'
 
+// Import PrimeIcons for icon library support
+import 'primeicons/primeicons.css'
+
+// Import root application component
 import App from './App.vue'
 
+// Create and mount the Vue application instance
 const app = createApp(App)
 
+// Mount the app to the target DOM element with id 'app'
 app.mount('#app')
